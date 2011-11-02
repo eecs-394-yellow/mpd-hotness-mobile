a3e56d60fd1109f82ef6fa0e03756198bdea0c26
Michael Jan <mikeyjan@gmail.com>
2011-11-02 03:17:52 -0500

    Finished script visual representation.

---
+++ assets/www/js/script.js
@@ -0,0 +1,32 @@
+(function (window, $) {
+
+
+$(document).ready(function() {
+
+  $('#slider').bind('change',function(event, data) {
+    value = $(this).val();
+
+    if (value==1) {
+        $('#heat').text('Mild');
+    }
+
+    if( value==2) {
+        $('#heat').text('Hot');
+    }
+
+    if (value==3) {
+        $('#heat').text('Fire');
+    }
+
+    if (value==4) {
+        $('#heat').text('Call the fire department!!!');
+    }
+	
+	if (value==5) {
+        $('#heat').text('RAAAGE!!!');
+    }
+	});
+});
+
+
+})(window, jQuery);
