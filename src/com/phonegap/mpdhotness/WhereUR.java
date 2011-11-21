2c1d36d9d703a92f6c698b05b8557753aef120ad
Ethan Romba <EthanRomba@u.northwestern.edu>
2011-11-21 05:27:15 -0600

    Upgraded to PhoneGap 1.2.0

--- src/com/phonegap/mpdhotness/WhereUR.java
+++ src/com/phonegap/mpdhotness/WhereUR.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.phonegap.*;
 
-public class App extends DroidGap {
-    /** Called when the activity is first created. */
+public class WhereUR extends DroidGap
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
     }
